2ba426ca5c7d79d166dd57c6fc07ccbe847e0dbf
unknown <kgoncharenko83@gmail.com>
2026-03-09 19:45:57 +0100

    Add SQL_manager

--- app/managers.py
+++ app/managers.py
@@ -3,4 +3,49 @@ import sqlite3
 from app.models import Actor
 
 
-# add manager here
+class ActorManager:
+    def __init__(self, db_name: str, table_name: str) -> None:
+        self.db_name = db_name
+        self.table_name = table_name
+        self.conn = sqlite3.connect(self.db_name)
+
+    def create(self, first_name: str, last_name: str) -> None:
+        query = (
+            f"""
+            INSERT INTO {self.table_name} (first_name, last_name)
+            VALUES (?, ?)
+            """
+        )
+        self.conn.execute(query, (first_name, last_name))
+        self.conn.commit()
+
+    def all(self) -> list:
+        query = (
+            f"""
+            SELECT first_name, last_name, id
+            FROM {self.table_name}
+            """
+        )
+        cursor = self.conn.execute(query)
+        return [Actor(*row) for row in cursor.fetchall()]
+
+    def update(self, pk: int, new_first_name: str, new_last_name: str) -> None:
+        query = (
+            f"""
+            UPDATE {self.table_name}
+            SET first_name = ?, last_name = ? WHERE id = ?
+            """
+        )
+        self.conn.execute(query, (new_first_name, new_last_name, pk))
+        self.conn.commit()
+
+    def delete(self, pk: int) -> None:
+        query = (
+            f"""
+            DELETE
+            FROM {self.table_name}
+            WHERE id = ?
+            """
+        )
+        self.conn.execute(query, (pk,))
+        self.conn.commit()
